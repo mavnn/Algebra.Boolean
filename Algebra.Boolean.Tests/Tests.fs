913a144187830b13ae858135d40ca76221990cde
mavnn <michael@mavnn.co.uk>
2014-09-26 15:16:51 +0100

    Working tests, xunit reference - with file add this time.

---
+++ Algebra.Boolean.Tests/Tests.fs
@@ -0,0 +1,8 @@
+﻿module Algebra.Boolean.Tests
+
+open Algebra.Boolean
+open Xunit
+
+[<Fact>]
+let ``Infrastructure test`` () =
+    Assert.Equal(<@@ 1 + 1 @@>, id' <@@ 1 + 1 @@>)
---
+++ Algebra.Boolean/Transforms.fs
@@ -0,0 +1,15 @@
+﻿module Algebra.Boolean
+
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.DerivedPatterns
+open Microsoft.FSharp.Quotations.ExprShape
+open Microsoft.FSharp.Quotations.Patterns
+
+let id' quote =
+    let rec transform q =
+        match q with
+        | ShapeVar v -> Expr.Var v
+        | ShapeLambda (v, e) -> Expr.Lambda(v, transform e)
+        | ShapeCombination (o, es) ->
+            ExprShape.RebuildShapeCombination(o, es)
+    transform quote
